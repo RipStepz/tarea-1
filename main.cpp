47ec0c1754081e3c8e351bfd86655ee65e97a2dd
RipStepz <ripstepz@gmail.com>
2024-04-06 18:13:41 -0300

    empezando

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    struct Torre {
+    char simbolo = 'T'; // Define qué tipo de pieza es y su caracter
+    int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
+};
+
+    
+    return 0;
+}
