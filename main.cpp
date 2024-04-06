69f786f3d9de0407d750db5bc9f92fc58f34c109
RipStepz <ripstepz@gmail.com>
2024-04-06 18:55:18 -0300

    archivo abierto

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
 #include <iostream>
+#include <fstream> 
 using namespace std;
-int main()
+
+int main() {
+
+struct Pieza 
 {
-    struct Torre {
-    char simbolo = 'T'; // Define qué tipo de pieza es y su caracter
-    int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
+
+ char simbolo; // Define qué tipo de pieza es y su caracter
+ int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
+
 };
 
-    
+struct Tablero 
+{
+
+ Pieza* piezas_tablero; // Lista de piezas que tiene el tablero
+
+};
+
+ifstream fich("no.txt");
+
+if (!fich.is_open())
+{
+
+cout<< "hubo un error al abrir el archivo UwU´nt" << endl;
+
+
+}
+
+else
+{
+
+cout<<"se abrio UwU" << endl;
+
+}
+
+fich.close();
+
     return 0;
 }
